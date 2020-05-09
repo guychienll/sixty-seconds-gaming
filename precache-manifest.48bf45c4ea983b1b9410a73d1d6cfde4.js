a89ef884b55c099b254033ea1ec475e60e1baa95
guychien <guychienll@gmail.com>
2020-05-09 22:29:59 +0800

    Updates

---
+++ precache-manifest.48bf45c4ea983b1b9410a73d1d6cfde4.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5eec65435cee04ab64f65e5f43ae1c71",
+    "url": "/sixty-seconds-gaming/index.html"
+  },
+  {
+    "revision": "fded974bf799355705c4",
+    "url": "/sixty-seconds-gaming/static/css/main.3b9a89c7.chunk.css"
+  },
+  {
+    "revision": "be3ad4e14c1f40ae0522",
+    "url": "/sixty-seconds-gaming/static/js/2.cad134f1.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/sixty-seconds-gaming/static/js/2.cad134f1.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "fded974bf799355705c4",
+    "url": "/sixty-seconds-gaming/static/js/main.11f3526c.chunk.js"
+  },
+  {
+    "revision": "6436041c8d4fea7e8da8",
+    "url": "/sixty-seconds-gaming/static/js/runtime-main.827f3f37.js"
+  }
+]);
